3b2d2d566c8d6d55624320f6a8682ea8869616bf
ktw11 <ktw1897@naver.com>
2025-03-02 18:30:08 +0900

    [Domain] AuthRepository Interface 추가

---
+++ Domain/Sources/Interface/AuthRepository.swift
@@ -0,0 +1,13 @@
+//
+//  AuthRepository.swift
+//  Domain
+//
+//  Created by 공태웅 on 3/2/25.
+//
+
+import Foundation
+
+public protocol AuthRepository: Sendable {
+    func signIn(token: String, account: String) async throws -> SignInResponse
+    func signIn(refreshToken: String) async throws -> SignInResponse
+}
