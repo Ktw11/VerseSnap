cd245f5a0b3387ced24a96bfa5a457e262f9d75c
Ktw11 <ktw1897@naver.com>
2025-03-02 15:41:20 +0900

    [Repository] SignInInfoLocalDataSource 생성 및 구현

---
+++ Data/Repository/Sources/DataSource/SignInInfoLocalDataSource.swift
@@ -0,0 +1,79 @@
+//
+//  SignInInfoLocalDataSource.swift
+//  Repository
+//
+//  Created by 공태웅 on 3/2/25.
+//
+
+import Foundation
+import SwiftData
+
+protocol SignInInfoDataSource {
+    func retrieve() async -> SignInInfoDTO?
+    func save(info: SignInInfoDTO) async throws
+    func reset() async throws
+}
+
+@ModelActor
+actor SignInInfoLocalDataSource: SignInInfoDataSource {
+    
+    // MARK: Lifecycle
+    
+    init() {
+        let schema = Schema([
+            PersistSignInInfo.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        
+        do {
+            let container = try ModelContainer(for: schema, configurations: [modelConfiguration])
+            let modelContext = ModelContext(container)
+            self.modelExecutor = DefaultSerialModelExecutor(modelContext: modelContext)
+            self.modelContainer = container
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+    
+    // MARK: Methods
+    
+    func retrieve() -> SignInInfoDTO? {
+        let descriptor = FetchDescriptor<PersistSignInInfo>()
+        return try? modelContext.fetch(descriptor).first?.toDTO
+    }
+
+    func save(info: SignInInfoDTO) throws {
+        try reset()
+        modelContext.insert(info.toPersist)
+        try modelContext.save()
+    }
+    
+    func reset() throws {
+        try modelContext.delete(model: PersistSignInInfo.self)
+    }
+}
+
+@Model
+private final class PersistSignInInfo {
+    @Attribute(.unique)
+    @Attribute(.allowsCloudEncryption)
+    var refreshToken: String
+    var signInType: String
+    
+    init(refreshToken: String, signInType: String) {
+        self.refreshToken = refreshToken
+        self.signInType = signInType
+    }
+}
+
+private extension PersistSignInInfo {
+    var toDTO: SignInInfoDTO {
+        .init(refreshToken: refreshToken, signInType: signInType)
+    }
+}
+
+private extension SignInInfoDTO {
+    var toPersist: PersistSignInInfo {
+        .init(refreshToken: refreshToken, signInType: signInType)
+    }
+}
---
+++ Data/Repository/Sources/Model/SignInInfoDTO.swift
@@ -0,0 +1,13 @@
+//
+//  SignInInfoDTO.swift
+//  Repository
+//
+//  Created by 공태웅 on 3/2/25.
+//
+
+import Foundation
+
+struct SignInInfoDTO {
+    let refreshToken: String
+    let signInType: String
+}
