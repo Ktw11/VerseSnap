04dda558ba96a570b95a23e8cb66b2425c5705af
ktw11 <ktw1897@naver.com>
2025-04-04 20:37:15 +0900

    [CommonUI] KeyboardObserver 추가 및 구현

---
+++ Shared/CommonUI/Sources/Keyboard/KeyboardObserver.swift
@@ -0,0 +1,46 @@
+//
+//  KeyboardObserver.swift
+//  CommonUI
+//
+//  Created by 공태웅 on 4/4/25.
+//
+
+import Foundation
+import UIKit
+import Combine
+
+@Observable
+@MainActor
+public final class KeyboardObserver: Sendable {
+    // MARK: Lifecyle
+    
+    public init() {
+        setUpEvents()
+    }
+    
+    // MARK: Properties
+    
+    public private(set) var currentHeight: CGFloat = 0
+    private var bag = Set<AnyCancellable>()
+}
+
+private extension KeyboardObserver {
+    func setUpEvents() {
+        Publishers.Merge(
+            NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+                .compactMap { $0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect }
+                .map { $0.height },
+            NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+                .map { _ in CGFloat(0) }
+        )
+        .eraseToAnyPublisher()
+        .sink { [weak self] height in
+            self?.update(to: height)
+        }
+        .store(in: &bag)
+    }
+    
+    func update(to height: CGFloat) {
+        self.currentHeight = height
+    }
+}
