6f55246e9a6a47b9f9717b851d899739ca4ac460
ktw11 <ktw1897@naver.com>
2025-05-04 20:28:59 +0900

    [Repository] 일기 배열을 저장하는 기능 추가

--- Data/Repository/Sources/DataSource/DiaryLocalDataSource.swift
+++ Data/Repository/Sources/DataSource/DiaryLocalDataSource.swift
@@ -10,6 +10,14 @@ import SwiftData
 
 public protocol DiaryLocalDataSource: Sendable {
     func save(_ diary: DiaryDTO) async throws
+    func save(_ diaries: [DiaryDTO]) async throws
+    
+    func retreiveDiariesByMonth(
+        startTimestamp: TimeInterval,
+        endTimestamp: TimeInterval,
+        after: TimeInterval?,
+        size: Int
+    ) async throws -> [DiaryDTO]
 }
 
 @ModelActor
@@ -34,15 +42,56 @@ public actor DiaryLocalDataSourceImpl: DiaryLocalDataSource {
     }
     
     public func save(_ diary: DiaryDTO) async throws {
-        try reset()
         modelContext.insert(diary.toPersist)
         try modelContext.save()
+    }
+    
+    public func save(_ diaries: [DiaryDTO]) async throws {
+        for diary in diaries {
+            modelContext.insert(diary.toPersist)
+        }
+        try modelContext.save()
+    }
+    
+    public func retreiveDiariesByMonth(
+        startTimestamp: TimeInterval,
+        endTimestamp: TimeInterval,
+        after: TimeInterval?,
+        size: Int
+    ) async throws -> [DiaryDTO] {
+        let predicate: Predicate<PersistDiary>
+        if let after = after {
+            predicate = #Predicate {
+                $0.createdAt >= startTimestamp &&
+                $0.createdAt < endTimestamp &&
+                $0.createdAt < after
+            }
+        } else {
+            predicate = #Predicate {
+                $0.createdAt >= startTimestamp &&
+                $0.createdAt < endTimestamp
+            }
+        }
+        
+        let fetchDescriptor = FetchDescriptor<PersistDiary>(
+            predicate: predicate,
+            sortBy: [.init(\.createdAt, order: .reverse),]
+        )
+        
+        let results = try modelContext.fetch(fetchDescriptor)
+        return Array(results.map { $0.toDTO }.prefix(size))
     }
 }
 
 private extension DiaryLocalDataSourceImpl {
     func reset() throws {
         try modelContext.delete(model: PersistDiary.self)
+    }
+}
+
+private extension PersistDiary {
+    var toDTO: DiaryDTO {
+        .init(id: id, imageURL: imageURL, hashtags: hashtags, createdAt: createdAt, verse: verse, isFavorite: isFavorite)
     }
 }
 
